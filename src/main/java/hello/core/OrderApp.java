107dfa9db2ede224a9898072f91b51856a46fd2b
dlwlckdWkd12 <jccc23@naver.com>
2023-06-15 20:43:38 +0900

    'custom_to_spring'

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,16 +7,24 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-            AppConfig appConfig = new AppConfig();
+//            AppConfig appConfig = new AppConfig();
+        //Config 안에 Bean 객체들                                                        //config Class
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        MemberService memberService = appConfig.memberService();
+                                                                    //메서드명와 맞춰야함    반환타입
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl(null);
 
-        OrderService orderService = appConfig.orderService();
+
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+//        OrderService orderService = appConfig.orderService();
 //        OrderService orderService = new OrderServiceImpl(null,null);
         
         Long memberId = 1L;
